8923938e323e58963009ab438382d5d3b1a281d5
Viresh Umbre <ibrahim.hasan@mail.utoronto.ca>
2018-07-27 21:28:24 -0400

    Set up app.js (finally)

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
+app.get("/game", function(req, res) {
+	res.sendFile(path.join(__dirname, '/views/', '/game.html'));
+});
+
 
 app.listen(3000, function() {
 	console.log("Server has started at PORT: 3000");
