142593dc2fbf2aef705af1914428027128af6598
Viresh Umbre <ibrahim.hasan@mail.utoronto.ca>
2018-07-28 15:58:43 -0400

    Added coin animations and multiple coins

--- games/levelTwo.js
+++ games/levelTwo.js
@@ -18,14 +18,15 @@ var config = {
 const game = new Phaser.Game(config);
 var cursors;
 var player;
-var showDebug = false;
+var coin;
+var count = 0;
 
 function preload ()
 {
     this.load.image("tiles", "/sprites/scenery/roguelikeSheet_transparent.png");
     this.load.tilemapTiledJSON("map", "/sprites/map/level2.json");
     this.load.image('dot', '/sprites/characters/reddot.png');
-    this.load.image('gold', '/sprites/characters/gold.png');
+    this.load.spritesheet('coin', '/sprites/characters/coin_spritesheet.png', {frameWidth: 14, frameHeight: 14});
 
 }
 
@@ -37,20 +38,41 @@ function create() {
     // Parameters: layer name (or index) from Tiled, tileset, x, y
     const belowLayer = map.createStaticLayer("belowPlayer", tileset, 0, 0),
           worldLayer = map.createStaticLayer("World", tileset, 0, 0);
-
     worldLayer.setCollisionByProperty({ collides: true });
-    gold = this.physics.add.staticGroup();
-    gold.create(230, 310, 'gold');
+
+    coins = this.physics.add.group();
+
+    coin1 = this.physics.add.sprite(230, 310, 'coin');
+    coin2 = this.physics.add.sprite(24, 260, 'coin');
+    coin3 = this.physics.add.sprite(311, 144, 'coin');
+
+    coins.add(coin1);
+    coins.add(coin2);
+    coins.add(coin3);
+    
 
     player = this.physics.add.sprite(12, 24, 'dot');
-    player.setCollideWorldBounds(true);
-
     player.setCollideWorldBounds(true);
     this.physics.add.collider(player, worldLayer);
 
     cursors = this.input.keyboard.createCursorKeys();
-    this.physics.add.overlap(player, gold, collectGold, null, this);
+    
 
+    this.anims.create({
+      key: 'coins',
+      frames: this.anims.generateFrameNumbers('coin', { start: 0, end: 3 }),
+      frameRate: 6,
+      repeat: -1 
+      });
+
+    coin1.anims.play('coins');
+    coin2.anims.play('coins');
+    coin3.anims.play('coins');
+
+    // this.physics.add.overlap(player, coin1, collectGold, null, this);
+    // this.physics.add.overlap(player, coin2, collectGold, null, this);
+    // this.physics.add.overlap(player, coin3, collectGold, null, this);
+    this.physics.add.overlap(player, coins, collectGold, null, this);
 }
 
     
@@ -66,8 +88,11 @@ function update () {
   }
 }
 
-function collectGold(player, gold) {
-  gold.disableBody(true, true);
-  window.location.replace("/paywallOne");
+function collectGold(player, coin) {
+  count += 1;
+  coin.disableBody(true, true);
+  if (count === 3) {
+    window.location.replace("/paywallTwo");
+  }
 }
 
