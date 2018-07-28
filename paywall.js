0d9ded81779bc13a6da60aae55895ea0948d82a1
davidli3100 <david.li3100@gmail.com>
2018-07-28 09:08:45 -0400

    pushing close/paywall init

--- paywall.js
+++ paywall.js
@@ -8,6 +8,16 @@ sprite.checkWorldBounds = true;
 
 playerSprite.events.onOutOfBounds.add(levelOnePaywall, this);
 
+/**
+ * modal initialization]
+ */
+
+ /* Instantiating iziModal */
+ $(".prompt-modal").iziModal({
+    overlayClose: false,
+    overlayColor: 'rgba(0, 0, 0, 0.6)'
+    });
+
 function levelOnePaywall() {
-    
+    $('.prompt-modal').iziModal('open');
 }
